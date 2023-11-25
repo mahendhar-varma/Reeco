81ab927486661aac50d407c9313e7ad28d5acd0e
mahendhar <7093741662mahiv@gmail.com>
2023-11-25 02:52:49 +0000

    cart default view added

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,9 +1,15 @@
+import { Div, Head, Para, Image } from "./styledComponent";
+
 const Cart = () => {
   return (
-    <>
-      <h1>Cart</h1>
-      <p>Your cart is empty</p>
-    </>
+    <Div>
+      <Head>Cart</Head>
+      <Para>Your cart is empty</Para>
+      <Image
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-empty-cart-img.png"
+        alt="no data"
+      />
+    </Div>
   );
 };
 
---
+++ src/components/Cart/styledComponent.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Head = styled.h1`
+  font-size: 23px;
+  margin-top: 0px;
+`;
+
+export const Para = styled.p`
+  font-size: 18px;
+  margin-top: 0px;
+  margin-bottom: 45px;
+`;
+
+export const Image = styled.img`
+  width: 200px;
+  height: 200px;
+`;
